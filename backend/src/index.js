a13f9dc89ddb46e3d749f9367e5cb5cce1496fdc
João Pedro Santos <betajpsg@gmail.com>
2020-05-02 19:57:46 -0300

    estruturado as pastas

---
+++ backend/src/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const cors = require('cors');
+const routes = require('./routes');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+//app.use(routes);
+
+
+
+app.listen(5000, console.log("Listen porta 3000")); 
---
+++ backend/src/routes.js
@@ -0,0 +1,10 @@
+const express = require('express')
+
+
+const routes = express.Router();
+
+/** Documentar as rotas
+ * 
+ */
+
+module.exports = routes;
